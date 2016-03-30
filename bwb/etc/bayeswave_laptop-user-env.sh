fbca577e3a1fc1d4edecdc85623181532c602a40
James Clark <james.clark@ligo.org>
2016-03-30 19:36:51 -0400

    include path

--- bwb/etc/bayeswave_laptop-user-env.sh
+++ bwb/etc/bayeswave_laptop-user-env.sh
@@ -3,3 +3,4 @@
 export PATH=${PATH}:${HOME}/src/lscsoft/bayeswave/trunk/src
 export LIBRARY_PATH=${HOME}/opt/lscsoft/lalsuite_master/lib:/usr/lib64:/usr/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=${HOME}/opt/lscsoft/lalsuite_master/lib:/usr/lib64:/usr/lib:$LIBRARY_PATH
+export C_INCLUDE_PATH=${HOME}/opt/lscsoft/lalsuite_master/include:$C_INCLUDE_PATH
