26e8f1b28d8a4545626a4b215948bb11948b9cc6
James Clark <james.clarrk@ligo.org>
2016-03-31 09:47:16 -0400

    adding amp order so that flow really does what we want

--- bwb/NRinjections/make_siminspiral.sh
+++ bwb/NRinjections/make_siminspiral.sh
@@ -11,12 +11,13 @@ outfile_with_fref="GaTechIMBBH_with_fref.xml"
 lalapps_inspinj \
     --seed 1234 --f-lower 30 --gps-start-time ${gpsstart} \
     --gps-end-time ${gpsend} --waveform NR_hdf5threePointFivePN \
+    --amp-order 0 \
     --ninja2-mass --nr-file ${catalogfile} \
     --time-step 1 --time-interval 0 --l-distr random \
     --dchirp-distr uniform --i-distr uniform \
-    --min-distance 1000 --max-distance 500000 \
+    --min-distance 50000 --max-distance 50000 \
     --m-distr nrwaves --disable-spin \
-    --min-mtotal 100. --max-mtotal 500.\
+    --min-mtotal 100. --max-mtotal 100.\
     --taper-injection start --output ${outfile}
 
 # add the fref
