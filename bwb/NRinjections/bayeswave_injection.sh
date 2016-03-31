715e9dd5b8ed3b4309be04bf4fa83aa30fc7ac71
James Clark <james.clark@ligo.org>
2016-03-31 02:17:31 -0400

    adding post command

--- bwb/NRinjections/bayeswave_injection.sh
+++ bwb/NRinjections/bayeswave_injection.sh
@@ -5,12 +5,18 @@ source ../etc/bayeswave_ldg-user-env.sh
 which bayeswave
 which bayeswave_post
 
-bayeswave \
+#   bayeswave \
+#       --ifo H1 --H1-flow 16 --H1-cache LALSimAdLIGO \
+#       --H1-channel LALSimAdLIGO  \
+#       --inj GaTechIMBBH.xml --event 0 \
+#       --srate 512 --seglen 4 \
+#       --trigtime 1126621184 \
+#       --PSDstart 1126621184 --PSDlength 1024 \
+#       --NCmin 2 --NCmax 2 --dataseed 1234 \
+#       --inj-numreldata /data/lvc_nr/GaTech/GT0901.h5
+
+bayeswave_post \
     --ifo H1 --H1-flow 16 --H1-cache LALSimAdLIGO \
-    --H1-channel LALSimAdLIGO  \
-    --inj GaTechIMBBH.xml --event 0 \
-    --srate 512 --seglen 4 \
-    --trigtime 1126621184 \
-    --PSDstart 1126621184 --PSDlength 1024 \
-    --NCmin 2 --NCmax 2 --dataseed 1234 \
-    --inj-numreldata /data/lvc_nr/GaTech/GT0901.h5
+    --H1-channel LALSimAdLIGO --trigtime 1126621184 \
+    --srate 512 --seglen 4 --PSDstart 1126621184\
+    --PSDlength 1024 --dataseed 1234 --0noise
