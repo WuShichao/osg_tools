137c674fe1195d8ca7fb31b6597a35e6d8b5cfda
James Clark <james.clarrk@ligo.org>
2016-04-06 14:28:30 -0400

    env script

--- bwb/etc/bayeswave_ldg-user-env.sh
+++ bwb/etc/bayeswave_ldg-user-env.sh
@@ -5,3 +5,8 @@ export LIBRARY_PATH=${HOME}/opt/lscsoft/lalsuite_master/lib:/usr/lib64:/usr/lib:
 export LD_LIBRARY_PATH=${HOME}/opt/lalsuite_master/lib:/usr/lib64:/usr/lib:$LIBRARY_PATH
 export C_INCLUDE_PATH=${HOME}/opt/lscsoft/lalsuite_master/include:$C_INCLUDE_PATH
 export PYTHONPATH=${PYTHONPATH}:${HOME}/src/lscsoft/bayeswave/trunk/utils
+
+export C_INCLUDE_PATH=${HOME}/opt/lscsoft/lalsuite_master/include:/usr/include:/usr/include/gsl:$C_INCLUDE_PATH
+export LIBRARY_PATH=${HOME}/opt/lscsoft/lalsuite_master/lib:/usr/lib64:$LIBRARY_PATH
+export LD_LIBRARY_PATH=${HOME}/opt/lscsoft/lalsuite_master/lib:/usr/lib64:$LIBRARY_PATH
+
