6132eaeb9df32b9afaff3bce367926ac23f26f7a
James Clark <james.clark@ligo.org>
2016-06-08 18:23:30 +0200

    GW150914 CWB followup

---
+++ bwb/testsuite/cwb_GW150914/run_pipe_cwb_GW150914.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Example call to the pipeline, using trigger time from offline CWB CED:
+# https://www.atlas.aei.uni-hannover.de/~vedovato/LSC/reports/G184098_ceds_run1/ced/ced_1126259272_800_G184098_ceds_run1_slag0_lag0_1_rMRA/L1H1_1126259461.500_1126259461.500/
+
+bwb_pipe.py cwb_GW150914-C02.ini \
+    --workdir cwb_GW150914-C02 \
+    --trigger-time 1126259462.392
