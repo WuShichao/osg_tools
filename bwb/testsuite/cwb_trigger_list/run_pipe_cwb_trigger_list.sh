cccad3033dfff09076c38a78f0bb2ca1b38c97cd
James Clark <james.clarrk@ligo.org>
2016-06-27 18:33:49 -0400

    removed demo timeslides (changed how they're handled) and added example trigger files

--- bwb/testsuite/cwb_trigger_list/run_pipe_cwb_trigger_list.sh
+++ bwb/testsuite/cwb_trigger_list/run_pipe_cwb_trigger_list.sh
@@ -3,4 +3,4 @@
 # Example call to the pipeline, using trigger list and sim data
 bwb_pipe.py cwb_trigger_list.ini \
     --workdir cwb_trigger_list \
-    --trigger-list trigger_times-and-freqs-and-rhos.asc
+    --trigger-list $1
