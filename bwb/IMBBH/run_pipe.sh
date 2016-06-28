c877652ec1ff76d4a234751e725284bf4a9997e1
James Clark <james.clark@ligo.org>
2016-06-28 13:50:49 -0700

    tidying

--- bwb/IMBBH/run_pipe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-../bwb_pipe.py topTenCWBIMBBH.ini \
-    --workdir topTenCWBIMBBH \
-    --user-tag topTenCWBIMBBH \
-    --trigger-list topTenCWB_mean.txt
--- bwb/use_cases/run_pipe_mdc_injection.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bwb_pipe.py mdc_injection.ini \
-    --workdir mdc_injection \
-    --trigger-time 966390501.02 \
-    --skip-datafind
