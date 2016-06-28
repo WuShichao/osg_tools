cb5e3ccfe2ae5f58de8e986c4f2d85e318532296
James Clark <james.clarrk@ligo.org>
2016-06-28 17:26:31 -0400

    minor

--- bwb/testsuite/cwb_trigger_list/run_pipe_cwb_trigger_list.sh
+++ bwb/testsuite/cwb_trigger_list/run_pipe_cwb_trigger_list.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #
-# Example call to the pipeline, using trigger list and sim data
+# Example call to the pipeline, using a CWB trigger list
 bwb_pipe.py config_triggers.ini \
     --workdir external_triggers \
-    --cwb-trigger-list $1
+    --cwb-trigger-list cwb_background.asc
+
+# Example call to the pipeline, using one of the other trigger lists
+bwb_pipe.py config_triggers.ini \
+    --workdir external_triggers \
+    --trigger-list trigger_times-and-lags-and-freqs.asc
