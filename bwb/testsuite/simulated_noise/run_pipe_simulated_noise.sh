d889e25936a9a05567dce6e1fe170cd15085ac0d
James Clark <james.clark@ligo.org>
2016-10-24 17:48:08 -0400

    updated

--- bwb/testsuite/simulated_noise/run_pipe_simulated_noise.sh
+++ bwb/testsuite/simulated_noise/run_pipe_simulated_noise.sh
@@ -2,5 +2,8 @@
 #
 # Example call to the pipeline, using trigger list and sim data
 bwb_pipe.py simulated_noise.ini \
-    --workdir simulated_noise \
-    --trigger-list trigger_list.asc
+    --workdir simulated_noise_single \
+    --trigger-time 1126258467.576778 \
+    --sim-data
+    #--condor-submit
+    #--trigger-list trigger_list.asc \
