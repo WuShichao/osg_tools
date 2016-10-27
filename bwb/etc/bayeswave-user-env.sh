cfcf685215cbc9e6a88a54f885c98076f17df695
James Clark <james.clark@ligo.org>
2016-10-27 16:39:37 -0400

    increasingly automated env scripts

--- bwb/etc/bayeswave-user-env.sh
+++ bwb/etc/bayeswave-user-env.sh
@@ -1,37 +1,64 @@
 #!/bin/bash
 
-# -------------------------------------------
+# ----------------------------------------- #
 #                   LAL                     #
-# -------------------------------------------
+# ----------------------------------------- #
 
-CVMFS="/cvmfs/oasis.opensciencegrid.org/ligo/pipeline/"
-if [ -e ${BAYESWAVE_CVMFS}/bayeswave/bayeswave_deps-user-env.sh ]
-then 
-    echo "BayesWave dependency setup detected in /cvmfs. Sourcing bayeswave_deps-user-env.sh"
-    source ${BAYESWAVE_CVMFS}/bayeswave/bayeswave_deps-user-env.sh
+if [ -d /cvmfs/oasis.opensciencegrid.org/ligo/pipeline ]
 
-elif [ -z ${LALSUITE_PREFIX} ]
+    echo "CVMFS detected, assuming OSG deployment"
+    OSG_DEPLOY=1
+    CVMFS_PIPEDIR=/cvmfs/oasis.opensciencegrid.org/ligo/pipeline
+
+    # Set up BayesWave dependencies
+    if [ -e ${CVMFS_PIPEDIR}/bayeswave/bayeswave_deps-user-env.sh ]
+    then 
+        echo "BayesWave dependency setup detected in /cvmfs. Sourcing bayeswave_osg_deps-user-env.sh"
+        source ${CVMFS_PIPEDIR}/bayeswave/bayeswave_osg_deps-user-env.sh
+    else
+        echo "!!! WARNING: no bayeswave dependency setup found in ${CVMFS} !!!"
+    fi
+
+    # Set LAL_DATA_PATH
+    echo "Setting LAL_DATA_PATH=${CVMFS_PIPEDIR}/pycbc/pycbc/ROM/lal-data/lalsimulation"
+    export LAL_DATA_PATH=${CVMFS_PIPEDIR}/pycbc/pycbc/ROM/lal-data/lalsimulation
+
+else
+    OSG_DEPLOY=0
+fi
+
+
+if [ -z ${LALSUITE_PREFIX} ]
+then
     echo "LALSUITE_PREFIX is set to ${LALSUITE_PREFIX}, using that installation"
     source ${LALSUITE_PREFIX}/etc/lalsuiterc
     source ${LALSUITE_PREFIX}/pylal/etc/pylal-user-env.sh
     source ${LALSUITE_PREFIX}/glue/etc/glue-user-env.sh
 else
-    echo "LALSUITE_PREFIX is unset, better hope there's a global installation"
+    echo "LALSUITE_PREFIX is unset, you better hope there's a global installation"
 fi
 
 
-# -------------------------------------------
-#                 LAL DATA                  #
-# -------------------------------------------
+# ----------------------------------------- #
+#                 LAL_DATA_PATH             #
+# ----------------------------------------- #
 
-if [ -d ${BAYESWAVE_CVMFS}/bayeswave_deps-user-env.sh ]
-then 
-    export LAL_DATA_PATH="/cvmfs/oasis.opensciencegrid.org/ligo/pipeline/pycbc/pycbc/ROM/lal-data/lalsimulation"
+if [ ${OSG_DEPLOY} ]
+then
+    export LAL_DATA_PATH=${CVMFS_PIPEDIR}/pycbc/pycbc/ROM/lal-data/lalsimulation
+    echo "Setting LAL_DATA_PATH=${LAL_DATA_PATH}"
+else
+    if [ -z ${LAL_DATA_PATH} ]
+    then 
+        echo "Using LAL_DATA_PATH=${LAL_DATA_PATH}"
+    else
+        echo "!!! WARNING: LAL_DATA_PATH is unset !!!"
+    fi
 fi
 
-# -------------------------------------------
+# ----------------------------------------- #
 #                 BAYESWAVE                 #
-# -------------------------------------------
+# ----------------------------------------- #
 
 # BayesWave
 export PATH=${PATH}:${HOME}/data/src/lscsoft/bayeswave/trunk/src
